c4f02388b705b49e89d63fdd68534099f89db16c
Piers Shepperson <piers.shepperson@btinternet.com>
2017-06-09 23:33:12 +0100

    Add install script to minial_map bat

--- minimal_map/deploy/buildWin_minimal_map.bat
+++ minimal_map/deploy/buildWin_minimal_map.bat
@@ -3,32 +3,36 @@ REM buildWin_minimal_map.bat
 REM Initilise builds
 call C:\"Program Files (x86)\Microsoft Visual Studio 14.0\VC"\vcvarsall.bat amd64
 
+REM source varables
+set PROJECTNAME=minimal_map
+set GITHUB_URL=https://github.com/Sriep/WebExamples.git
+
 REM Set varables
-set BUILD_DIR=C:\Data\Build
+set BUILD_DIR=C:\Data\Build_%PROJECTNAME%
+set SRCDIR=%BUILD_DIR%\WebExamples\%PROJECTNAME%
 set QT_DIR=C:\Qt\5.9\msvc2015_64
 set PATH=%QT_DIR%\bin;%PATH%
 set WINDEPLOYQT_EXE=%QT_DIR%\bin\windeployqt.exe
 set INNO_DIR="C:\Program Files (x86)\Inno Setup 5"
-set SRCDIR=%BUILD_DIR%\WebExamples\minimal_map
 
-REM Clear build directory
-REM del /s /q  %BUILD_DIR%
-REM rd %BUILD_DIR% /s /q 
-REM rmdir %BUILD_DIR% /s /q
+REM Setup build directory
 mkdir %BUILD_DIR%
 cd %BUILD_DIR%
 
 REM Download from GitHub
-git clone https://github.com/Sriep/WebExamples.git
+git clone %GITHUB_URL%
 
 REM build
 cd %SRCDIR%
-qmake.exe %SRCDIR%\minimal_map.pro -spec win32-msvc "CONFIG+=qtquickcompiler" && jom.exe qmake_all
+qmake.exe %SRCDIR%\%PROJECTNAME%.pro -spec win32-msvc "CONFIG+=qtquickcompiler" && jom.exe qmake_all
 jom.exe 
 jom.exe clean
 
 REM Build release
 set RELEASE_DIR=%SRCDIR%\release
 set QML_DIR=%SRCDIR%
-%WINDEPLOYQT_EXE% --qmldir %QML_DIR% %RELEASE_DIR%\minimal_map.exe
+%WINDEPLOYQT_EXE% --qmldir %QML_DIR% %RELEASE_DIR%\%PROJECTNAME%.exe
 
+REM Build setup filescd %INNO_DIR%
+cd %INNO_DIR%
+iscc /Q %SRCDIR%\deploy\%PROJECTNAME%.iss
